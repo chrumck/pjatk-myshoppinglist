af6981a5c4d72d458e05fe81e6996c8d0241295a
Tomasz Dobrowolski <tdobrowolski@geocomp.com>
2018-11-01 08:21:57 -0400

    Add ProductListActivity

---
+++ app/src/main/java/com/chrumck/pjatk/myshoppinglist/ProductListActivity.java
@@ -0,0 +1,28 @@
+package com.chrumck.pjatk.myshoppinglist;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.Objects;
+
+public class ProductListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar
+                .make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
