573aa5d526efc647e257fb46b4fcf421ae5a229b
Tomasz Dobrowolski <tdobrowolski@geocomp.com>
2018-11-01 15:51:23 -0400

    Add settings

--- app/src/main/java/com/chrumck/pjatk/myshoppinglist/MainActivity.java
+++ app/src/main/java/com/chrumck/pjatk/myshoppinglist/MainActivity.java
@@ -2,17 +2,24 @@ package com.chrumck.pjatk.myshoppinglist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
         setContentView(R.layout.activity_main);
+
 
         findViewById(R.id.btn_showShoppingList).setOnClickListener(v ->
                 startActivity(new Intent(this, ProductListActivity.class)));
+
+        findViewById(R.id.btn_showSettings).setOnClickListener(v ->
+                startActivity(new Intent(this, SettingsActivity.class)));
     }
 }
--- app/src/main/java/com/chrumck/pjatk/myshoppinglist/ProductListActivity.java
+++ app/src/main/java/com/chrumck/pjatk/myshoppinglist/ProductListActivity.java
@@ -11,16 +11,17 @@ public class ProductListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_product_list);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> Snackbar
                 .make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
