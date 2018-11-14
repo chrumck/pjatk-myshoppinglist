6e519c2c8e0b086d82fdc60068c528eb396c5684
Tomasz Dobrowolski <tdobrowolski@geocomp.com>
2018-11-14 18:58:46 -0500

    Remove notification handling from NotificationActionReceiver

--- app/src/main/java/com/chrumck/pjatk/myshoppinglist/NotificationActionReceiver.java
+++ app/src/main/java/com/chrumck/pjatk/myshoppinglist/NotificationActionReceiver.java
@@ -1,6 +1,5 @@
 package com.chrumck.pjatk.myshoppinglist;
 
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,17 +7,9 @@ import android.util.Log;
 
 public class NotificationActionReceiver extends BroadcastReceiver {
 
-    public static final String NOTIFICATION_ID = "NOTIFICATION_ID";
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("NotificationActionRec", "received intent: " +
-                intent.getAction() + " id:" + intent.getIntExtra(NOTIFICATION_ID, 0));
-
-        context.sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
-
-        NotificationManager notifManager = (NotificationManager) context.getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-        notifManager.cancel(intent.getIntExtra(NOTIFICATION_ID, 0));
+        Log.i("NotificationActionRec", "received intent: " + intent.getAction());
 
         if (intent.getAction().equals(context.getResources().getString(R.string.app_action_product_list))) {
             context.startActivity(new Intent(context, ProductListActivity.class));
