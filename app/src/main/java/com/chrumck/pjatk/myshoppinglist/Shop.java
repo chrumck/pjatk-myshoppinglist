fc0f63918682dfff220da01188f26cddb02b226d
Tomasz Dobrowolski <tdobrowolski@geocomp.com>
2019-01-04 07:55:54 -0500

    Add google places lookup to ShopEditActivity

--- app/src/main/java/com/chrumck/pjatk/myshoppinglist/Shop.java
+++ app/src/main/java/com/chrumck/pjatk/myshoppinglist/Shop.java
@@ -4,15 +4,17 @@ public class Shop {
     public String id;
     public String name;
     public String description;
+    public String url;
     public ShopLocation location;
 
     public Shop() {
     }
 
-    public Shop(String id, String name, String description) {
+    public Shop(String id, String name, String description, String url) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.url = url;
     }
 
     public static class ShopLocation {
--- app/src/main/java/com/chrumck/pjatk/myshoppinglist/ShopEditActivity.java
+++ app/src/main/java/com/chrumck/pjatk/myshoppinglist/ShopEditActivity.java
@@ -1,21 +1,39 @@
 package com.chrumck.pjatk.myshoppinglist;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.RuntimeExecutionException;
 
 public class ShopEditActivity extends BaseActivity {
     private DbHelper dbHelper;
+    private PlaceDetectionClient placeDetectionClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         dbHelper = new DbHelper(this);
+        placeDetectionClient = Places.getPlaceDetectionClient(this);
 
         setContentView(R.layout.shop_edit_activity);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -23,6 +41,7 @@ public class ShopEditActivity extends BaseActivity {
 
         EditText name = findViewById(R.id.edit_shop_name);
         EditText description = findViewById(R.id.edit_shop_description);
+        EditText url = findViewById(R.id.edit_shop_url);
         EditText latitude = findViewById(R.id.edit_shop_latitude);
         EditText longitude = findViewById(R.id.edit_shop_longitude);
         EditText radius = findViewById(R.id.edit_shop_radius);
@@ -38,6 +57,7 @@ public class ShopEditActivity extends BaseActivity {
 
                 name.setText(shop.name);
                 description.setText(shop.description);
+                url.setText(shop.url);
 
                 if (shop.location == null) {
                     return;
@@ -57,7 +77,8 @@ public class ShopEditActivity extends BaseActivity {
             Shop newShop = new Shop(
                     shopId,
                     name.getText().toString(),
-                    description.getText().toString());
+                    description.getText().toString(),
+                    url.getText().toString());
 
             if (!TextUtils.isEmpty(latitude.getText()) &&
                     !TextUtils.isEmpty(longitude.getText()) &&
@@ -85,5 +106,63 @@ public class ShopEditActivity extends BaseActivity {
         });
 
         findViewById(R.id.edit_shop_btn_cancel).setOnClickListener(v -> finish());
+
+        findViewById(R.id.edit_shop_btn_find_nearest).setOnClickListener(v -> {
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+                return;
+            }
+
+            int servicesStatus = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+            if (servicesStatus != ConnectionResult.SUCCESS) {
+                Toast.makeText(this, "Play Services not available: " + servicesStatus, Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            showProgressDialog("Searching...");
+
+            placeDetectionClient.getCurrentPlace(null).addOnCompleteListener(task -> {
+                hideProgressDialog();
+
+                PlaceLikelihoodBufferResponse likelyPlaces = null;
+                try {
+                    likelyPlaces = task.getResult();
+                } catch (RuntimeExecutionException exception) {
+                    Toast.makeText(this, "Failed retrieving places: " + exception.getMessage(), Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (likelyPlaces.iterator() == null || !likelyPlaces.iterator().hasNext()) {
+                    Toast.makeText(this, "Nothing found", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                    Log.i("Places", String.format("Place '%s' has likelihood: %g",
+                            placeLikelihood.getPlace().getName(),
+                            placeLikelihood.getLikelihood()));
+                }
+
+                Place place = likelyPlaces.iterator().next().getPlace();
+
+                description.setText(place.getName());
+
+                Uri uri = place.getWebsiteUri();
+                if (uri != null) {
+                    url.setText(uri.toString());
+                }
+
+                LatLng latLng = place.getLatLng();
+                if (latLng != null) {
+                    latitude.setText(Double.toString(latLng.latitude));
+                    longitude.setText(Double.toString(latLng.longitude));
+                    radius.setText(Float.toString(10));
+                }
+
+                likelyPlaces.release();
+            });
+        });
     }
 }
